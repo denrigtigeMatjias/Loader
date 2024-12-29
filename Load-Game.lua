475199ddd6801f8c6d65e477daa88725f5267bf3
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2024-12-29 23:54:34 +0100

    Create Load-Game.lua

---
+++ Load-Game.lua
@@ -0,0 +1,4 @@
+if game.PlaceId == 116434053579571 then
+  script_key=getgenv().script_key;
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2aa203ddfd55bb7a2544699ea01c31e2.lua"))()
+end
