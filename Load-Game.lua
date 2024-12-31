93408b6570c9821e8c3a52fbf42407db9b9b29e8
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2024-12-31 02:03:41 +0100

    Update Load-Game.lua

--- Load-Game.lua
+++ Load-Game.lua
@@ -1,7 +1,9 @@
 if game.PlaceId == 116434053579571 then
+  -- Mining GO!
   script_key=getgenv().script_key;
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2aa203ddfd55bb7a2544699ea01c31e2.lua"))()
 elseif game.PlaceId == 1537690962 then
+  -- Bee Swarm Simulator
   script_key=getgenv().script_key;
   loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/20cc0111794ae8f58681a22475b508e5.lua"))()
 else
